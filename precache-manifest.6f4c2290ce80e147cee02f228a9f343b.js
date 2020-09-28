fa22d1eae801bbbf3f1358984a6b30a33809e751
Alexander M <alexander.maehl@gmail.com>
2020-09-28 14:42:01 +1300

    Updates

---
+++ precache-manifest.6f4c2290ce80e147cee02f228a9f343b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0358efa085951601c43a70743b1b032b",
+    "url": "/single-page-site/index.html"
+  },
+  {
+    "revision": "75d7fb7bef37f841c520",
+    "url": "/single-page-site/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "52e5160901fe47cf5b06",
+    "url": "/single-page-site/static/js/2.84e57e79.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/single-page-site/static/js/2.84e57e79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75d7fb7bef37f841c520",
+    "url": "/single-page-site/static/js/main.62873317.chunk.js"
+  },
+  {
+    "revision": "4f0429e72f91b43884c5",
+    "url": "/single-page-site/static/js/runtime-main.1f5746ac.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/single-page-site/static/media/logo.5d5d9eef.svg"
+  }
+]);
